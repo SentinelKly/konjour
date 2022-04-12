4d5a4da1162d5b126c5f13aabcc8964af55fd0b4
SentinelKly <100457265+SentinelKly@users.noreply.github.com>
2022-04-12 08:24:01 -0700

    New debug build name

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir bin
-gcc src/konjour.c src/cfg_parser.c src/gcc_builder.c src/err_handler.c -o ./bin/test
+gcc src/konjour.c src/cfg_parser.c src/gcc_builder.c src/err_handler.c -o ./bin/konjour-dbg
