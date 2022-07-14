28d4b0f1f1849efe468de306d1380863f118e2f4
SentinelKly <100457265+SentinelKly@users.noreply.github.com>
2022-07-14 02:35:37 -0400

    Basic parsing finished

--- src/konjour.cc
+++ src/konjour.cc
@@ -1,13 +1,118 @@
 #include "konjour.hh"
 
-BuildTable::BuildTable()
-{
+static const std::string LOG_LEVELS[] = {"ERROR: ", "WARN: ", "INFO: "};
 
+static const std::string FIELDS[] = 
+{
+	"c_std", "cxx_std", "output", "cflags", "lflags", "binary", "mode",
+	"inc_paths", "lib_paths", "sources", "defines", "libs"
+};
+
+void logMessage(LogType type, const std::string& message, ...)
+{
+	char buffer[999] = {};
+	std::va_list parseArgs;
+		
+	va_start(parseArgs, message);
+	std::vsnprintf(buffer, 999, message.c_str(), parseArgs);
+	va_end(parseArgs);
+
+	std::cout << LOG_LEVELS[(uint8) type] << buffer << std::endl;
 }
 
-int main()
+std::string Artefact::toString()
 {
-	const auto art = new Artefact("test");
-	std::cout << art->m_Name << std::endl;
+	char8 buffer[999] = {0};
+
+	sprintf(buffer, "name: %s\nc standard: %s\ncxx standard: %s\n mode: %s\n", 
+	m_StringFields.find(FieldType::C_STD)->second),
+	m_StringFields.find(FieldType::CXX_STD)->second,
+	m_StringFields.find(FieldType::MODE)->second;
+
+	return buffer;
+}
+
+BuildTable::~BuildTable()
+{
+	for (auto const& art : this->m_SortedArtefacts)
+		delete art.second;
+
+	m_SortedArtefacts.clear();
+}
+
+void BuildTable::sortArtefactsIntoMap()
+{
+	for (auto const& art : this->m_Artefacts)
+		this->m_SortedArtefacts[art.second->m_Priority] = art.second;
+
+	m_Artefacts.clear();
+}
+
+void BuildTable::addArtefact(Artefact *art)
+{
+	this->m_Artefacts[art->m_Name] = art;
+}
+
+void BuildTable::parseConfiguration(std::string& path)
+{
+	toml::color::enable();
+	
+	try 
+	{
+		auto config = toml::parse(path);
+		std::string globalCompiler = toml::find_or<std::string>(config, "KONJOUR_COMPILER", "gcc");
+		std::vector<std::string> artefactList = toml::find<std::vector<std::string>>(config, "artefacts");
+
+		if (artefactList.size() < 1) 
+		{
+			std::cerr << toml::format_error("[error] no artefacts are defined!",
+			config.at("artefacts"), "at least one artefact required.") << std::endl;
+			
+			this->m_ErrorFlag = true;
+		}
+
+		for (auto i : artefactList)
+		{
+			const auto& arteTable = toml::find(config, i);
+
+			auto art = new Artefact(i);
+			art->m_StringFields[FieldType::C_STD]   = toml::find_or<std::string>(arteTable, FIELDS[(uint8) FieldType::C_STD]   , "11");
+			art->m_StringFields[FieldType::CXX_STD] = toml::find_or<std::string>(arteTable, FIELDS[(uint8) FieldType::CXX_STD] , "11");
+			art->m_StringFields[FieldType::OUTPUT]  = toml::find_or<std::string>(arteTable, FIELDS[(uint8) FieldType::OUTPUT]  , "bin");
+			art->m_StringFields[FieldType::CFLAGS]  = toml::find_or<std::string>(arteTable, FIELDS[(uint8) FieldType::CFLAGS]  , "");
+			art->m_StringFields[FieldType::LFLAGS]  = toml::find_or<std::string>(arteTable, FIELDS[(uint8) FieldType::LFLAGS]  , "");
+			art->m_StringFields[FieldType::BINARY]  = toml::find_or<std::string>(arteTable, FIELDS[(uint8) FieldType::BINARY]  , "executable");
+			art->m_StringFields[FieldType::MODE]    = toml::find_or<std::string>(arteTable, FIELDS[(uint8) FieldType::MODE]    , "debug");
+
+			if (arteTable.contains(FIELDS[(uint8) FieldType::INC_PATHS]))
+				art->m_VectorFields[FieldType::INC_PATHS] = toml::find<std::vector<std::string>>(arteTable, FIELDS[(uint8) FieldType::INC_PATHS]);
+			
+			if (arteTable.contains(FIELDS[(uint8) FieldType::INC_PATHS]))
+				art->m_VectorFields[FieldType::LIB_PATHS] = toml::find<std::vector<std::string>>(arteTable, FIELDS[(uint8) FieldType::LIB_PATHS]);
+			
+			art->m_VectorFields[FieldType::SOURCES]   = toml::find<std::vector<std::string>>(arteTable, FIELDS[(uint8) FieldType::SOURCES]);
+
+			if (arteTable.contains(FIELDS[(uint8) FieldType::INC_PATHS]))
+				art->m_VectorFields[FieldType::DEFINES]   = toml::find<std::vector<std::string>>(arteTable, FIELDS[(uint8) FieldType::DEFINES]);
+
+			if (arteTable.contains(FIELDS[(uint8) FieldType::INC_PATHS]))
+				art->m_VectorFields[FieldType::LIBS]      = toml::find<std::vector<std::string>>(arteTable, FIELDS[(uint8) FieldType::LIBS]);
+		}
+	}
+	
+	catch (std::exception& e) 
+	{
+		LOG_ERROR(e.what());
+		this->m_ErrorFlag = true;
+	}
+}
+
+int32 main(int32 argc, char8 **argv)
+{
+	std::string configPath = (argc > 1) ? argv[1] : "./konjour.toml";
+
+	auto table = new BuildTable();
+	table->parseConfiguration(configPath);
+	delete table;
 	return 0;
 }
--- src/konjour.hh
+++ src/konjour.hh
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <cstdarg>
 #include <map>
 
 #include "../vendor/toml.hpp"
@@ -18,6 +19,12 @@ using uint16 = unsigned short;
 using uint32 = unsigned int;
 using uint64 = unsigned long long;
 
+using char8  = char;
+
+#define LOG_ERROR(x, ...) logMessage(LogType::ERROR, x, ##__VA_ARGS__)
+#define LOG_WARN(x, ...)  logMessage(LogType::WARN , x, ##__VA_ARGS__)
+#define LOG_INFO(x, ...)  logMessage(LogType::INFO , x, ##__VA_ARGS__)
+
 enum class FieldType: uint8
 {
 	//std::string fields
@@ -27,20 +34,44 @@ enum class FieldType: uint8
 	INC_PATHS, LIB_PATHS, SOURCES, DEFINES, LIBS
 };
 
+enum class LogType : int8 
+{
+	ERROR, WARN, INFO
+};
+
+void logMessage(LogType type, const std::string& message, ...);
+
 struct Artefact
 {
 	public:
+		std::unordered_map<FieldType, std::string> m_StringFields;
+		std::unordered_map<FieldType, std::vector<std::string>> m_VectorFields;
+
 		std::string m_Name;
-		std::map<FieldType, std::string> m_StringFields;
-		std::map<FieldType, std::vector<std::string>> m_VectorFields;
+		uint64 m_Priority = 0;
 
 	public:
-		Artefact(std::string&& name) : m_Name{name}{};
+		Artefact(std::string& name) : m_Name{name}{};
+		~Artefact() = default;
+
+		std::string toString();
 };
 
 class BuildTable
 {
 	private:
+		std::map<uint64, Artefact *> m_SortedArtefacts;
+		std::unordered_map<std::string, Artefact *> m_Artefacts;
+		std::string m_Compiler;
+
+		bool m_ErrorFlag;
+
 	public:
-		BuildTable();
+		BuildTable() = default;
+		~BuildTable();
+
+		void addArtefact(Artefact *art);
+		void sortArtefactsIntoMap();
+
+		void parseConfiguration(std::string& path);
 };
